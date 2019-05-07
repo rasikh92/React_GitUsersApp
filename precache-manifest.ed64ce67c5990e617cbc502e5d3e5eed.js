301b2b96bfbbdf054e2ea4b8f4bee6f57530a8d6
Rasikh Mukaram <rasikh@192-168-1-156.tpgi.com.au>
2019-05-07 14:20:04 +1000

    Updates

---
+++ precache-manifest.ed64ce67c5990e617cbc502e5d3e5eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57537c94307d64c0195af05c093ba6ce",
+    "url": "/React_GitUsersApp/index.html"
+  },
+  {
+    "revision": "51ecff5fd8a96a84e540",
+    "url": "/React_GitUsersApp/static/css/main.c52aae07.chunk.css"
+  },
+  {
+    "revision": "20d94db5a848ef25f06f",
+    "url": "/React_GitUsersApp/static/js/2.6f64500a.chunk.js"
+  },
+  {
+    "revision": "51ecff5fd8a96a84e540",
+    "url": "/React_GitUsersApp/static/js/main.11052153.chunk.js"
+  },
+  {
+    "revision": "e9b8ef61472501cac7f0",
+    "url": "/React_GitUsersApp/static/js/runtime~main.230f791b.js"
+  },
+  {
+    "revision": "14f7c70e5be40b0b48b862f6db3b384d",
+    "url": "/React_GitUsersApp/static/media/Octocat.14f7c70e.png"
+  }
+]);
